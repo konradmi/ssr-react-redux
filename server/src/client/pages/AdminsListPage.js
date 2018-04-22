8efc25cd2ba3d538503890e0a8b146238c7597b5
KonradM <miekus.konrad@gmail.com>
2018-04-22 14:01:03 +0200

    Handle unauthorised users correctly

--- server/src/client/pages/AdminsListPage.js
+++ server/src/client/pages/AdminsListPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchAdmins } from '../actions'
+import requireAuth from '../components/hocs/requireAuth'
 
 class AdminsListPage extends Component {
   componentDidMount() {
@@ -32,6 +33,6 @@ function loadData(store) {
 }
 
 export default {
-  component: connect(mapStateToProps, { fetchAdmins })(AdminsListPage),
+  component: connect(mapStateToProps, { fetchAdmins })(requireAuth(AdminsListPage)),
   loadData
 }
